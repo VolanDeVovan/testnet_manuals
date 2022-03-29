c380476a3b0fbcb59773745abaf7740d084c97e2
Vladimir Kravchenko <volandevovan2004@gmail.com>
2022-03-29 19:57:48 +0300

    Add starknet

---
+++ starknet/README.md
@@ -0,0 +1,23 @@
+# starknet node setup
+For starknet node you have to create account on alchemy.com and create application 
+
+1. Register on alchemy.com
+2. Create application 
+Chain: Ethereum
+Network: Mainnet / Goeril
+3. Copy http link 
+Example: https://eth-goerli.alchemyapi.io/v2/...
+
+
+## installation binary
+Installation can take more than 10 minutes, it is recommended to run in a screen session:
+```
+screen -S starknet
+```
+
+Use script below for a quick installation:
+After run you have to paste the copied link you got on alchemy.com 
+
+```
+wget -O starknet.sh https://raw.githubusercontent.com/VolanDeVovan/testnet_manuals/main/starknet/starknet.sh && chmod +x starknet.sh && ./starknet.sh
+```
---
+++ starknet/starknet.sh
@@ -0,0 +1,64 @@
+echo "=================================================="
+echo -e "\e[1m\e[32m1. Preparation before installation... \e[0m" && sleep 1
+echo -e "\e[1m\e[32mPaste your link from alchemy: \e[0m" && sleep 1
+
+read alchemy_link
+
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m2. Updating dependencies... \e[0m" && sleep 1
+sudo apt update
+sudo apt -y upgrade
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m3. Installing required dependencies... \e[0m" && sleep 1
+sudo apt -y install curl git
+sudo apt -y install python3 python3-venv python3-dev
+sudo apt -y install build-essential libgmp-dev pkg-config libssl-dev
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
+
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m4. Setting up Starknet fullnode ... \e[0m" && sleep 1
+cd $HOME
+rm -rf starknet
+
+git clone --branch v0.1.6-alpha https://github.com/eqlabs/pathfinder.git
+
+cd $HOME/pathfinder/py
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+
+
+source $HOME/.cargo/env
+cargo build --release --bin pathfinder
+cp $HOME/pathfinder/target/release/pathfinder /usr/local/bin/
+
+echo "[Unit]
+Description=StarkNet
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+WorkingDirectory=$HOME/pathfinder/py
+ExecStart=/bin/bash -c \"source $HOME/pathfinder/py/.venv/bin/activate && /usr/local/bin/pathfinder --http-rpc=\"0.0.0.0:9545\" --ethereum.url $alchemy_link\"
+Restart=on-failure
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target" > $HOME/starknetd.service
+
+mv $HOME/starknetd.service /etc/systemd/system/
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable starknetd
+sudo systemctl restart starknetd
